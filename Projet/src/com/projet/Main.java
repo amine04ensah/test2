a919668e29c4e3b321c85b3007dc23610f16e15b
Jacques <Jacques@Jacques-PC>
2017-01-25 00:24:26 +0000

    new project for Github

---
+++ Projet/src/com/projet/Main.java
@@ -0,0 +1,12 @@
+package com.projet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" vive le maroc");
+		System.out.println(" j'aime le maroc");
+
+	}
+
+}
